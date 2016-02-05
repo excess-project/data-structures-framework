dfca73345e657caca9f282633faa0b6d3ede7e44
Anders Gidenstam <anders@gidenstam.org>
2016-02-05 14:20:46 +0100

    SpGEMM: Updated the PBS job script.

--- PBS/SpGEMM_job/make_jobs.sh
+++ PBS/SpGEMM_job/make_jobs.sh
@@ -4,12 +4,13 @@
 THREADS="2 4 6 8 10 12 14 16 18 20"
 # Test max threads
 #THREADS="20"
-# All algorithms
-ALGORITHMS="0 1 2 3 4 5 6 7 8 9 10 11 12"
+# All algorithms (included in each job).
+#ALGORITHMS="0 1 2 3 4 5 6 7 8 9 10 11 12"
 # Just queues
 #ALGORITHMS="0 1 2 3 4 5 6 7 8"
 for T in $THREADS; do
-    for I in $ALGORITHMS; do
-        sed -e "s#CASETHREADS#$T#" -e "s#CASEALG#$I#" job > job.a$I.t$T
-    done
+    sed -e "s#CASETHREADS#$T#"  job > job.t$T
+#    for I in $ALGORITHMS; do
+#        sed -e "s#CASETHREADS#$T#" -e "s#CASEALG#$I#" job > job.a$I.t$T
+#    done
 done
