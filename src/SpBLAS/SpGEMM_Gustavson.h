0731b26a249cee424d2a7d3191dd550dc9014081
Anders Gidenstam <anders@gidenstam.org>
2016-03-31 13:39:39 +0200

    Added the selected license: GPL v3+ and for selected parts LGPL v3+.

--- src/SpBLAS/SpGEMM_Gustavson.h
+++ src/SpBLAS/SpGEMM_Gustavson.h
@@ -1,6 +1,22 @@
 // Sparse double matrix GEMM implementation based on the classical algorithm
-// in [Gustavson, ACM TMS 4(3), 1978].
-//   Copyright (C) 2015 - 2016  Anders Gidenstam
+// in [Gustavson, ACM TMS 4(3), 1978] and
+// [Gilbert et al., SIAM JMAA 13(1), 1992].
+//
+// Copyright (C) 2015 - 2016  Anders Gidenstam
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #ifndef __SPGEMM_GUSTAVSON_H
 #define __SPGEMM_GUSTAVSON_H
